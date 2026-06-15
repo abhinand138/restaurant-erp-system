19942405f0b455cd9250a4639ac023814f72e044
abhinand <abhinandma007@gmail.com>
2026-06-15 17:43:30 +0530

    Day 1 frontend setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+
+      <div className="bg-white p-10 rounded-xl shadow-lg text-center">
+
+        <h1 className="text-4xl font-bold text-blue-600">
+          Restaurant ERP
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          MCA Final Year Project
+        </p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
